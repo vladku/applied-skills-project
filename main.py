1efcc8f284832533775f809a8058c5fff89a2f3d
Vladyslav Kurovskyi <vladyslav.kurovskyi@takeoff.com>
2021-09-27 15:20:00 +0300

    init

--- main.py
+++ main.py
@@ -5,7 +5,26 @@ from google.cloud import firestore
 
 app = Flask(__name__)
 
-db = firestore.Client(project='roi-takeoff-user79')
+# db = firestore.Client(project='roi-takeoff-user79')
+# users_ref = db.collection(u'users').document(u'user')
+# docs = users_ref.get()
+# print(next(docs))
+# print([doc.to_dict() for doc in docs])
+
+# Add a new document
+db = firestore.Client()
+doc_ref = db.collection(u'users').document(u'alovelace')
+doc_ref.set({
+    u'first': u'Ada',
+    u'last': u'Lovelace',
+    u'born': 1815
+})
+
+# Then query for documents
+users_ref = db.collection(u'users')
+
+for doc in users_ref.stream():
+    print(u'{} => {}'.format(doc.id, doc.to_dict()))
 
 @app.route("/")
 def hello_world():
@@ -14,24 +33,25 @@ def hello_world():
 
 @app.route("/users")
 def users():
-    users_ref = db.collection(u'users')
-    docs = users_ref.stream()
-    return [doc.to_dict() for doc in docs]
+    return {"a": 123}
+    # users_ref = db.collection(u'users')
+    # docs = users_ref.stream()
+    # return {"result": [doc.to_dict() for doc in docs]}
 
-@app.route("/users/<id>", methods = ['GET', 'POST', 'DELETE'])
-def user(id):
-    if request.method == 'GET':
-        users_ref = db.collection(u'users')
-        docs = users_ref.stream()
-        return [doc.to_dict() for doc in docs]
-    if request.method == 'POST':
-        doc_ref = db.collection(u'users').document(u'user')
-        doc_ref.set({
-            u'first': u'Alan',
-            u'middle': u'Mathison',
-            u'last': u'Turing',
-            u'born': 1912
-        })
+# @app.route("/users/<id>", methods = ['GET', 'POST', 'DELETE'])
+# def user(id):
+#     if request.method == 'GET':
+#         users_ref = db.collection(u'users')
+#         docs = users_ref.stream()
+#         return [doc.to_dict() for doc in docs]
+#     if request.method == 'POST':
+#         doc_ref = db.collection(u'users').document(u'user')
+#         doc_ref.set({
+#             u'first': u'Alan',
+#             u'middle': u'Mathison',
+#             u'last': u'Turing',
+#             u'born': 1912
+#         })
 
 
 if __name__ == "__main__":
