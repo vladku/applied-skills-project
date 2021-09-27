3c4db2b2bdb1414e5532b0365bb61efbd304a9b0
Vladyslav Kurovskyi <vladyslav.kurovskyi@takeoff.com>
2021-09-27 12:35:47 +0300

    init

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
+from google.cloud import firestore
 
 app = Flask(__name__)
 
+db = firestore.Client(project='roi-takeoff-user79')
 
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
+
+@app.route("/users")
+def users():
+    users_ref = db.collection(u'users')
+    docs = users_ref.stream()
+    return [doc.to_dict() for doc in docs]
+
+@app.route("/users/<id>", methods = ['GET', 'POST', 'DELETE'])
+def user(id):
+    if request.method == 'GET':
+        users_ref = db.collection(u'users')
+        docs = users_ref.stream()
+        return [doc.to_dict() for doc in docs]
+    if request.method == 'POST':
+        doc_ref = db.collection(u'users').document(u'user')
+        doc_ref.set({
+            u'first': u'Alan',
+            u'middle': u'Mathison',
+            u'last': u'Turing',
+            u'born': 1912
+        })
 
 
 if __name__ == "__main__":
